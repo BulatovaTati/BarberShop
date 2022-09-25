35ff44f13de7fe389868953b1445b6ea3c718267
BulatovaTati <kohodatat@gmail.com>
2022-09-25 18:30:08 +0300

    add folders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './js/modal';
+// import './js/animation';
+// import './js/swiper';
+
+// import './js/menu';
+// import './js/modal';
+// import './js/to-top';
+
+// import './js/readmore-prod';
+// import './js/readmore-about';
+// import './js/smooth-scroll';
+// import './js/form';
