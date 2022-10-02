06fb2d71dc457adccb5a6749c013332471a9ffff
BulatovaTati <kohodatat@gmail.com>
2022-10-02 16:37:06 +0300

    add modal in section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-// import './js/animation';
-// import './js/swiper';
-
 import './js/menu';
 import './js/modal';
 import './js/to-top';
 import './js/agreement';
 
-// import './js/smooth-scroll';
 // import './js/form';
